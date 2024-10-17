5bbca9db88116db5248ba2b6cbae902e965ef84c
YoloFmaster <yolofmaste@gmail.com>
2024-10-17 11:07:11 +0300

    init

---
+++ config/secret.php
@@ -0,0 +1,6 @@
+<?php 
+const COOKIE_VALIDATION_KEY = "ikgoeg";
+const DB_NAME = "pushkov_DataBaseOptFIRPO";
+const DB_PASSWORD = "Mobpsyho100&";
+const DB_USERNAME = "pushkov_pushkov_firpo";
+?>
---
+++ models/User.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "User".
+ *
+ * @property int $id_user
+ * @property string $email
+ * @property string $phone
+ * @property string $login
+ * @property string $password
+ *
+ * @property Order[] $orders
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'User';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'phone', 'login', 'password'], 'required'],
+            [['email', 'phone', 'login', 'password'], 'string', 'max' => 255],
+            [['login'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_user' => 'Id User',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'login' => 'Login',
+            'password' => 'Password',
+        ];
+    }
+
+    /**
+     * Gets query for [[Orders]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrders()
+    {
+        return $this->hasMany(Order::class, ['user_id' => 'id_user']);
+    }
+
+    //IDENTITYINTERFACE PKGH{
+        public static function tableName()
+        {
+            return 'user';
+        }
+    
+        /**
+         * Finds an identity by the given ID.
+         *
+         * @param string|int $id the ID to be looked for
+         * @return IdentityInterface|null the identity object that matches the given ID.
+         */
+        public static function findIdentity($id)
+        {
+            return static::findOne($id);
+        }
+    
+        /**
+         * Finds an identity by the given token.
+         *
+         * @param string $token the token to be looked for
+         * @return IdentityInterface|null the identity object that matches the given token.
+         */
+        public static function findIdentityByAccessToken($token, $type = null)
+        {
+
+        }
+    
+        /**
+         * @return int|string current user ID
+         */
+        public function getId()
+        {
+            return $this->id;
+        }
+    
+        /**
+         * @return string current user auth key
+         */
+        public function getAuthKey()
+        {
+
+        }
+    
+        /**
+         * @param string $authKey
+         * @return bool if auth key is valid for current user
+         */
+        public function validateAuthKey($authKey)
+        {
+
+        }
+    //}PKGH
+
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username'=> $username]);
+    }
+}
